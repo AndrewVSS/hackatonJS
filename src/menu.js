f3f09534a8d2ab5a07f9f17a901d705c3f21baee
AndewV <a.vanchugov@icloud.com>
2025-05-30 20:24:39 +0300

    context menu 1.1

--- src/menu.js
+++ src/menu.js
@@ -1,20 +1,34 @@
 import {Menu} from './core/menu'
 
 export class ContextMenu extends Menu {
+	constructor(selector) {
+		super(selector)
+		this.modules = [
+		  // тут будут импортированные модули
+		]
+	  }
+  
 	setup() {
-	  window.addEventListener('contextmenu', e => {
-		e.preventDefault()
-		const clickX = e.clientX
-		const clickY = e.clientY
-  
-		this.el.style.display = 'block'
-		this.el.style.left = `${clickX}px`
-		this.el.style.top = `${clickY}px`
-		console.log(this.el)
-	  })
-	}
+		window.addEventListener('contextmenu', e => {
+		  e.preventDefault()
+		  const clickX = e.clientX
+		  const clickY = e.clientY
 	
-  
+		  this.open(clickX, clickY) 
+		})
+	
+		document.body.addEventListener('click', () => this.close())
+	  }
+	
+	  open(x, y) {
+		this.el.innerHTML = ''
+		this.modules.forEach(mod => this.el.appendChild(mod.render()))
+		this.el.style.left = `${x}px`
+		this.el.style.top = `${y}px`
+		
+	  }
+	
+
 	close() {
 		this.el.style.display = 'none'
 	}
