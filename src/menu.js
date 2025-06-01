46855f66947aededf766b386e85d8845e7c22a47
Philipp Anufriev <afd42r@gmail.com>
2025-06-01 12:05:43 +0700

    Create module Click

--- src/menu.js
+++ src/menu.js
@@ -1,6 +1,7 @@
 import {Menu} from './core/menu'
 import { BackgroundModule } from './modules/background.module'
 import { ShapeModule } from './modules/shape.module'
+import { ClicksModule } from './modules/clicks.module'
 
 export class ContextMenu extends Menu {
 	constructor(selector) {
@@ -8,6 +9,7 @@ export class ContextMenu extends Menu {
 		this.modules = [
 			new BackgroundModule(),
 			new ShapeModule(),
+			new ClicksModule()
 		  	// тут будут импортированные модули
 		]
 	  }
